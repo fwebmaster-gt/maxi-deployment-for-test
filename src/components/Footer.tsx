68cc4169097cc87d647c8607041e1d7c1aaa228b
fwebmaster-gt <fwebmaster.gt@gmail.com>
2025-07-29 07:29:25 -0600

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -75,7 +75,7 @@ export const Footer = () => {
           </div>
         </div>
         <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-300">
-          <p>&copy; 2024 MAXICOMPRA. Todos los derechos reservados.</p>
+          <p>&copy; {new Date().getFullYear()} MAXICOMPRA. Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
